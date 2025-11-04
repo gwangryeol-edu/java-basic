26324ee116994b548ed44c884a320f5af086f680
gwangryeol-edu <rhkr0427rhkr@gmail.com>
2025-11-04 09:36:09 +0900

    P7-MaxNum 로직 변경

--- src/Java_basic_practice/P7_MaxNum.java
+++ src/Java_basic_practice/P7_MaxNum.java
@@ -2,12 +2,13 @@ package Java_basic_practice;
 
 public class P7_MaxNum {
     public static void main(String[] args) {
-        int a = 10, b = 25, c = 17;
+        int a = 10, b = 25, c = 170;
         int max = a;
 
         if (b > max) {
             max = b;
-        } else {
+        }
+        if (c > max) {
             max = c;
         }
         System.out.println(max);
